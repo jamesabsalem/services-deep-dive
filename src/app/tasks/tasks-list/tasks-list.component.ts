89d905c65627bf7d24061c0b169e1ceba6bedcf2
James Absalem Lintu <jamesabsalem@gmail.com>
2025-04-10 14:56:18 +0600

    video-177

--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,16 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { TaskService } from '../tasks.service'; // Adjust the path as necessary
 
 import { TaskItemComponent } from './task-item/task-item.component';
 
 @Component({
-    selector: 'app-tasks-list',
-    templateUrl: './tasks-list.component.html',
-    styleUrl: './tasks-list.component.css',
-    imports: [TaskItemComponent]
+  selector: 'app-tasks-list',
+  templateUrl: './tasks-list.component.html',
+  styleUrl: './tasks-list.component.css',
+  imports: [TaskItemComponent],
 })
 export class TasksListComponent {
+  private tasksService = inject(TaskService);
   selectedFilter = signal<string>('all');
-  tasks = [];
+  tasks = this.tasksService.allTasks;
 
   onChangeTasksFilter(filter: string) {
     this.selectedFilter.set(filter);
